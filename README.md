5c568ef76ccc8d2c2240a4bdb4aca13cdd55fce4
mdemon420 <mdeamon4@gmail.com>
2021-05-05 01:07:43 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# chainging-css-style
---
+++ index.js
@@ -0,0 +1,13 @@
+
+ var myVar = document.querySelector("#peraId");
+ 
+function addStyle(){
+   
+    myVar.classList.add("pera-style");
+ 
+}
+function removeStyle(){
+  
+    myVar.classList.remove("pera-style");
+}
+
